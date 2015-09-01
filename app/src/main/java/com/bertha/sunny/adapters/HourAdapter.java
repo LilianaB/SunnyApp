8f05e243e535688751ead775fb60bdc92d704e4f
lbarrios <liliana.usb@gmail.com>
2015-09-01 17:00:25 +0200

    recyclerView for hourly forecast working properly

--- app/src/main/java/com/bertha/sunny/adapters/HourAdapter.java
+++ app/src/main/java/com/bertha/sunny/adapters/HourAdapter.java
@@ -1,6 +1,7 @@
 package com.bertha.sunny.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -14,19 +15,32 @@ import com.bertha.sunny.weather.Hour;
  */
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
 
+    private Hour[] mHours;
+
+    public HourAdapter(Hour[] hours) {
+        mHours = hours;
+    }
+
     @Override
     public HourViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+        //view is root view or item layout
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.hourly_list_item, viewGroup, false);
+        HourViewHolder viewHolder = new HourViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(HourViewHolder hourViewHolder, int i) {
+        //bridge between adapter and viewholder
+        hourViewHolder.bindHour(mHours[i]);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mHours.length;
     }
 
     public class HourViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/bertha/sunny/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/bertha/sunny/ui/HourlyForecastActivity.java
@@ -4,26 +4,47 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.bertha.sunny.R;
+import com.bertha.sunny.adapters.HourAdapter;
 import com.bertha.sunny.weather.Hour;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class HourlyForecastActivity extends AppCompatActivity {
 
     private Hour[] mHours;
+
+    @Bind(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter hourAdapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(hourAdapter);
+
+        //new about recyclingView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        //to help performance
+        mRecyclerView.setHasFixedSize(true);
     }
 
 
